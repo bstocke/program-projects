078fbfadfda5db01054014bb62e66a2324ff16f4
bstocke <46466296+bstocke@users.noreply.github.com>
2021-03-22 16:57:46 -0400

    Add files via upload

---
+++ Module 8 Lab/sandbox.js
@@ -0,0 +1,51 @@
+//This pulls the data based on paragraph tag
+//const para = document.querySelector('p');
+
+//This pulls the error based on the div tag
+//const para = document.querySelector('div.errors');
+
+//One way of pulling the data directly from the array
+//const paras = document.querySelectorAll('p');
+//console.log(paras[1]);
+
+//This is a loop to access all of the data on the screen
+/*paras.forEach(para => {
+    console.log(para);
+});
+*/
+
+//This is for showing all elements and description vaules
+/*
+const paras = document.querySelectorAll('p');
+const errors = document.querySelectorAll('.error');
+console.log(errors);
+*/
+
+//Performing a GetElementByld
+const title = document.getElementById('page-title');
+const errors = document.getElementsByClassName('error');
+//console.log(errors[0]);
+
+//Try to do a ForEach in This Methodology should be prohibited
+/*
+errors.array.forEach(element => {
+    console.log(error);
+});
+*/
+
+//This uses a method to use InnerText and append an existing tag base
+/*const para = document.querySelector('p');
+para.innerText += ' Today is an awesome day';
+console.log(para.innerText);
+*/
+
+//const para = document.querySelector('p');
+
+//Selects all data with a paragrapgh tag
+/*const paras = document.querySelectorAll('p');
+
+paras.forEach(para => {
+    console.log(para.innerText);
+})
+*/
+
